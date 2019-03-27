2b3963ff777d0bedb4825ed0f53bda54054089e5
Josh <morrisjosh792@gmail.com>
2019-03-26 22:56:04 -0400

    Create GarageSetupActivity

---
+++ app/src/main/java/com/example/parkinggarage/activities/GarageSetupActivity.java
@@ -0,0 +1,26 @@
+package com.example.parkinggarage.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.parkinggarage.R;
+
+public class GarageSetupActivity extends AppCompatActivity {
+    private static final String TAG = "GarageSetupActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_garage_setup);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+    }
+
+}
